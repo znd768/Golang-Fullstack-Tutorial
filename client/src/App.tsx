d0fbfd25e17e89a506990a0377967e691047d200
znd768 <rasksk37@gmail.com>
2025-01-27 22:56:37 +0900

    set bg color on entire page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,15 @@
-import { Stack } from '@chakra-ui/react'
-import Navbar from './components/Navbar'
-import TodoForm from './components/TodoForm'
+import { Stack } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
+import TodoForm from "./components/TodoForm";
+import { useColorModeValue } from "@chakra-ui/color-mode";
 
 function App() {
-  return (
-    <Stack h="100vh">
-      <Navbar/>
-      <TodoForm />
-    </Stack>
-  )
+    return (
+        <Stack h="100vh" bg={useColorModeValue("gray.100", "gray.900")}>
+            <Navbar />
+            <TodoForm />
+        </Stack>
+    );
 }
 
-export default App
+export default App;
