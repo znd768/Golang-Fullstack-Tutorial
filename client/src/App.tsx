d57560a455b485eeb275d1a83160a9e4a1fab382
znd768 <rasksk37@gmail.com>
2025-01-27 21:48:08 +0900

    created form component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import { Stack } from '@chakra-ui/react'
 import Navbar from './components/Navbar'
+import TodoForm from './components/TodoForm'
 
 function App() {
   return (
     <Stack h="100vh">
       <Navbar/>
+      <TodoForm />
     </Stack>
   )
 }
---
+++ client/src/components/TodoForm.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { Button, Flex, Input, Spinner } from "@chakra-ui/react";
+import { IoMdAdd } from "react-icons/io";
+
+export default function TodoForm() {
+    const [newTodo, setNewTodo] = useState("");
+    const [isPending, setIsPending] = useState(false);
+    const createTodo = async (e: React.FormEvent) => {
+        e.preventDefault();
+        alert("Todo added!");
+    };
+    return (
+        <form onSubmit={createTodo}>
+            <Flex gap={2}>
+                <Input
+                    type="text"
+                    value={newTodo}
+                    onChange={(e) => setNewTodo(e.target.value)}
+                    ref={(input) => input && input.focus()}
+                />
+                <Button>{isPending ? <Spinner size={"xs"} />: <IoMdAdd size={30} />}</Button>
+            </Flex>
+        </form>
+    );
+}
