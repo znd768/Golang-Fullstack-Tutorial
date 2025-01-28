8fd56cb5490d5f7a5fef68bceb007abecf5df370
znd768 <rasksk37@gmail.com>
2025-01-28 21:10:10 +0900

    installed tanstack

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,13 +3,17 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { ColorModeProvider } from "@chakra-ui/color-mode";
 import { Provider } from "./components/ui/provider.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Provider>
-      <ColorModeProvider>
-        <App />
-      </ColorModeProvider>
-    </Provider>
-  </StrictMode>
+    <StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <Provider>
+                <ColorModeProvider>
+                    <App />
+                </ColorModeProvider>
+            </Provider>
+        </QueryClientProvider>
+    </StrictMode>
 );
