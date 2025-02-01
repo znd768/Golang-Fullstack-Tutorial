894c42be0ffce0c819de81428a4b9b906cb6d777
znd768 <rasksk37@gmail.com>
2025-02-02 02:43:18 +0900

    created delete function

--- main.go
+++ main.go
@@ -52,7 +52,7 @@ func main() {
 	app.Get("/api/todos", getTodos)
 	app.Post("/api/todos", createTodos)
 	app.Patch("/api/todos/:id", updateTodos)
-	// app.Delete("/api/todos/:id", deleteTodos)
+	app.Delete("/api/todos/:id", deleteTodos)
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -114,7 +114,20 @@ func updateTodos(c *fiber.Ctx) error {
 	}
 	return c.Status(200).JSON(fiber.Map{"success": true})
 }
-// func deleteTodos(c *fiber.Ctx) error {}
+
+func deleteTodos(c *fiber.Ctx) error {
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid todo ID"})
+	}
+	filter := bson.M{"_id":objectID}
+	_, err = collection.DeleteOne(context.Background(), filter)
+	if err != nil {
+		return err
+	}
+	return c.Status(200).JSON(fiber.Map{"success": true})
+}
 
 
 // prev version
